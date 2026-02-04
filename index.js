b2316472fee149444c1336f596bfbfe270fc56bd
berkaycelik1 <cberkay959@gmail.com>
2026-02-04 21:36:41 +0300

    GÜNCEL

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const postController = require('./controllers/postController');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json);
+
+app.get('/posts', postController.getPosts);
+
+app.listen(PORT, () => {
+    console.log("Sunucu çalışıyor... Port: " + PORT);
+
+});
