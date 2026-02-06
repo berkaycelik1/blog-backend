e4f0553cdb2cee15b09a81a1ab6d47fa54dc6050
berkaycelik1 <cberkay959@gmail.com>
2026-02-06 13:50:22 +0300

    güncel

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,8 +1,10 @@
 const postService = require('../services/postService');
 
 const getPosts = (req, res) => {
+    console.log("‼️ DİKKAT: Biri /post adresine girdi");
     try {
         const posts = postService.getAllPosts();
+        console.log("VERİLER:", posts);
 
         res.status(200).json(posts);
     } catch (error){
