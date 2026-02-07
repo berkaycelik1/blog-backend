667b08800aed6a1eac9592e4776b4926110af2d6
berkaycelik1 <cberkay959@gmail.com>
2026-02-08 00:11:36 +0300

    controller✅

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,55 +1,24 @@
-const AppDataSource = require("../data-source");
-const User = require("../entity/User");
+const authService = require("../services/authService");
+const { AppSuccessResponse, AppFailResponse } = require("../utils/response");
 
 const register = async (req, res) => {
     try {
-        const { fullName, email, password} = req.body;
-        const userRepository = AppDataSource.getRepository(User);
-        const existingUser = await userRepository.findOneBy({ email: email});
-
-        if (existingUser) {
-            return res.status(400).json({ message: "Bu email zaten kayıtlı!" });
-        }
-
-        const newUser = userRepository.create({
-            fullName: fullName,
-            email: email,
-            password: password,
-        });
-
-        await userRepository.save(newUser);
-
-        res.status(201).json({ message: "Kayıt başarılı!", user: newUser });
-
+        const{ fullName, email, password } = req.body;
+        const user = await authService.register(fullName, email, password);
+        res.status(201).json(new AppSuccessResponse("Kayıt başarılı!", user));
     } catch (error) {
-        console.error("Kayıt Hatası:", error);
-        res.status(500),json({ message: "Sunucu hatası oluştu."});
-        }
-    };
-    
-        const login = async (req, res) => {
-        try {
-            const {email, password} = req.body;
+        res.status(400).json(new AppFailResponse(error.message));
+    }
+};
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await authService.login(email, password);
 
-            console.log("Gelen Mail:", email);
-            console.log("Gelen Şire:", password);
+        res.status(200).json(new AppSuccessResponse("Giriş başarılı", user));
+    } catch (error) {
+        res.status(401).json(new AppFailResponse(error.message));
+    }
+};
 
-            const userRepository = AppDataSource.getRepository(User);
-            const user = await userRepository.findOneBy({ email: email});
-
-            if (!user) {
-                return res.status(404).json({ message: "Kullanıcı bulunamadı!" });
-            }
-            if (user.password !== password) {
-                return res.status(401).json({ message: "Hatalı şifre!" });
-            }
-
-            res.status(200).json({ message: "Giriş başarılı", user: user });
-
-            } catch (error) {
-                console.error("Hata:", error);
-            }
-        };
-
-        module.exports = { register, login};
-
+module.exports = { register, login};
