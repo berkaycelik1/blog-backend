8c6acf09d4fa8bce6d60c0cc6cf8e4eab6641cfa
berkaycelik1 <cberkay959@gmail.com>
2026-02-09 12:15:58 +0300

    garson güncel✅

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -1,16 +1,13 @@
-const postService = require('../services/postService');
+const postService = require("../services/postService");
+const { AppSuccessResponse, AppFailResponse} = require("../utils/response");
 
-const getPosts = (req, res) => {
-    res.json(postService.getAllPosts());
-};
-
-const getPostById = (req, res) => {
-    const post = postService.getPostById(req.params.id);
-    if (post) {
-        res.json(post);
-    } else {
-        res.status(404).json({ message: "Bulunamadı" });
+const createPost = async (req, res) => {
+    try {
+        const { userId, title, content } = req.body;
+        const newPost = await postService.createPost(userId, title, content);
+        res.status(201).json(new AppSuccessResponse("Post başarıyla oluşturuldu!",newPost));
+    } catch (error) {
+        res.status(400).json(new AppFailResponse(error.message));
     }
 };
-
-module.exports = { getPosts, getPostById };
+module.exports = { createPost };
